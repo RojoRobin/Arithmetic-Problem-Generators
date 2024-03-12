57c28b5794418fb8b22a96ba33b08646ec67e054
Robin <162392805+RojoRobin@users.noreply.github.com>
2024-03-12 18:41:23 -0400

    Adding multiplication practice

---
+++ multiplication.py
@@ -0,0 +1,17 @@
+import random
+myProblems=[1,2,3,4,5,6,7,8,9,10,11,12]
+with open("multiplicationPractice.txt", "w") as h:
+	with open("multiplication_AnswerKey.txt", "w") as j:
+		for i in range(1,20):
+			x = random.randrange(1,100,1)
+			y = random.randrange(1,100,1)
+			answerProduct = x * y
+			#print(f' {x} * {y} = {answerProduct}')
+			h.write(f' {x} * {y} = ___')
+			h.write("\n")
+			h.write("\n")
+			h.write("\n")
+			j.write(f' {x} * {y} = {answerProduct}')
+			j.write("\n")
+			j.write("\n")
+			j.write("\n")
